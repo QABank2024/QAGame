20f0ed4b37f3d2ba266b50b86dea65bb09b40075
= <=>
2024-10-31 14:02:56 +0000

    Git check 2

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -9,7 +9,7 @@ public class GameEngine {
 
         System.out.print("Enter number of monsters: ");
         int numMonsters = scanner.nextInt();
-        // Checking git
+        // Checking git2
         Game game = new Game(gridSize, numMonsters);
         game.startGame();
     }
